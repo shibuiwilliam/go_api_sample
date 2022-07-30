7a2d86de7f0de73765e89923ad7d8d3f34466861
shibuiwilliam <shibuiyusuke@gmail.com>
2022-07-30 11:13:48 +0900

    update

--- rest/src/main.go
+++ rest/src/main.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/sys/unix"
 )
 
 type Ping struct {
@@ -48,7 +51,35 @@ func main() {
 		Handler: router,
 		Addr:    httpPort,
 	}
-	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		log.Fatal(err)
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+		return nil
+	})
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, unix.SIGTERM)
+	select {
+	case <-sigCh:
+		log.Println("received SIGTERM, exiting gracefully")
+	case <-ctx.Done():
 	}
+
+	log.Println("gracefully shutdown http server")
+	if err := server.Shutdown(ctx); err == nil {
+		log.Println("completed http server graceful shutdown")
+	} else {
+		log.Fatalf("failed to shutdown http server", err)
+	}
+
+	cancel()
+	if err := eg.Wait(); err != nil {
+		log.Fatalf("unhandled error received", err)
+	}
+
+	log.Println("exiting with 0")
 }
